fc3f3dcb08722d011794185e9b6018ae59bb4dd1
Horst Kan <zxdfgcv@gmail.com>
2022-08-24 09:49:28 +0800

    Update 2022-08-24T01:49:17.071Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -3,9 +3,34 @@
 $(function () {
   console.log('Hello Bootstrap5');
 });
+"use strict";
+
+$(function () {
+  console.log('Hello Bootstrap5');
+});
 ClassicEditor.create(document.querySelector('#editor')).then(function (editor) {
   console.log(editor);
 })["catch"](function (error) {
   console.error(error);
+});
+"use strict";
+
+var swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+  effect: "fade",
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets'
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+  controller: {
+    inverse: true
+  }
 });
 //# sourceMappingURL=all.js.map
